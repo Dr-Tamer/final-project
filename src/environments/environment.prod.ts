dca8071194906d367b44602117f1f9b218801768
Jianzhao Li <github@lijianzhao.com>
2020-01-28 21:03:18 -0700

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  properties: {
+    title: '',
+    logo: 'assets/favicon.ico',
+    backgroundImg: 'assets/background.png',
+    categoryRegistry: 'assets/categories.json',
+    contentRegistry: 'assets/registry.json',
+    contentRoot: 'assets/contents',
+    displayNum: 8
+  },
+  production: true
+};
